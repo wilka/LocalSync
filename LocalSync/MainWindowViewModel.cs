68947680786a651a27ffb275949c62cd09908186
Wilka Hudson <wilka.hudson@gmail.com>
2020-01-14 20:34:05 +0000

    Add project files.

---
+++ LocalSync/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.IO.Abstractions;
+using ReactiveUI;
+
+namespace LocalSync
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public MainWindowViewModel(IDirectoryInfoFactory directoryInfoFactory, IFileInfoFactory fileInfoFactory)
+        {
+            SyncFolders = new ObservableCollection<SyncFolderPairViewModel>();
+
+            var foldersFile = fileInfoFactory.FromFileName(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "LocalSyncFolders.txt"));
+
+            using var reader = foldersFile.OpenText();
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split('|');
+                if (parts.Length != 2)
+                {
+                    throw new Exception("Each line in the settings file must be a pair of folders separated by `|`");
+                }
+
+                SyncFolders.Add(new SyncFolderPairViewModel
+                {
+                    Left = directoryInfoFactory.FromDirectoryName(parts[0].Trim()),
+                    Right = directoryInfoFactory.FromDirectoryName(parts[1].Trim()),
+                });
+            }
+        }
+
+        public ObservableCollection<SyncFolderPairViewModel> SyncFolders { get; }
+    }
+}
---
+++ LocalSync/SyncFolderPairViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.IO.Abstractions;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace LocalSync
+{
+    public class SyncFolderPairViewModel : ReactiveObject
+    {
+        public string Name => $"`{Left.FullName}` vs `{Right.FullName}`";
+
+        public IDirectoryInfo Left {get; set; }
+        public IDirectoryInfo Right {get; set; }
+
+        public ICommand Compare => ReactiveCommand.Create(() => 
+        { 
+            Process.Start(@"C:\Program Files\Beyond Compare 4\BCompare.exe", $"\"{Left.FullName}\" \"{Right.FullName}\""); 
+        });
+    }
+}
